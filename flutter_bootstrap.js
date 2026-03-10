092c386b2744aaa1df54a92118cd472767eb85ea
AdamPetrik <174123829+nxscz@users.noreply.github.com>
2026-03-10 21:12:26 +0100

    Daily free

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "333013886"
+    serviceWorkerVersion: "1203217652"
   }
 });
