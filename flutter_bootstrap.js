4d8d8b62c2b1271d896c9f23961203333ebe2c6a
AdamPetrik <174123829+nxscz@users.noreply.github.com>
2026-03-18 13:24:59 +0100

    Shop screen + overview page edit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2501970668"
+    serviceWorkerVersion: "3310666084"
   }
 });
